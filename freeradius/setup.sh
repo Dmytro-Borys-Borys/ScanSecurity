1d02f4d3887bc3b979025842de3ea36be4e5ff93
Dima <dmytro.borysborys@iesmiguelherrero.com>
2023-06-01 23:52:02 +0200

    NoDogSplash, Splashpage, Ticket

--- freeradius/setup.sh
+++ freeradius/setup.sh
@@ -25,15 +25,74 @@ clients_conf="$SCRIPT_DIR/clients.conf"
 write_template_to_file "$template" "$clients_conf"
 create_symbolic_link "$clients_conf" "/etc/freeradius/3.0/clients.conf" "freerad"
 
-sqlite_db="$SCRIPT_DIR/freeradius.db"
+schema_file="/etc/freeradius/3.0/mods-config/sql/main/sqlite/schema.sql"
+
+# Check if the database file already exists
+if [[ -f "$RADIUS_DB" ]]; then
+    echo "Database file already exists."
+else
+		# Check if the schema file exists
+	if [[ ! -f "$schema_file" ]]; then
+		echo "Schema file not found: $schema_file"
+	else
+		# Create the SQLite database and execute the schema.sql file
+		 sudo -u freerad sqlite3 "$RADIUS_DB" <<EOF
+.read "$schema_file"
+EOF
+
+		# Check if the schema.sql file was executed successfully
+		if [[ $? -eq 0 ]]; then
+			#sudo chown freerad:freerad $RADIUS_DB
+			echo "Database bootstrap successful."
+		else
+			echo "Error bootstrapping the database."
+			exit 1
+		fi	
+	fi
+fi
+
+
+
+
+
 
 template="sql {
 	dialect = \"sqlite\"
 	driver = \"rlm_sql_\${dialect}\"
 	sqlite {
-		filename = \"$sqlite_db\"
+		filename = \"${RADIUS_DB}\"
 		busy_timeout = 200
 		bootstrap = \"\${modconfdir}/\${..:name}/main/sqlite/schema.sql\"
+	}
+
+
+	mysql {
+		tls {
+			ca_file = \"/etc/ssl/certs/my_ca.crt\"
+			ca_path = \"/etc/ssl/certs/\"
+			certificate_file = \"/etc/ssl/certs/private/client.crt\"
+			private_key_file = \"/etc/ssl/certs/private/client.key\"
+			cipher = \"DHE-RSA-AES256-SHA:AES128-SHA\"
+			tls_required = yes
+			tls_check_cert = no
+			tls_check_cert_cn = no
+		}
+		warnings = auto
+	}
+	postgresql {
+		send_application_name = yes
+	}
+	mongo {
+		appname = \"freeradius\"
+		tls {
+			certificate_file = /path/to/file
+			certificate_password = \"password\"
+			ca_file = /path/to/file
+			ca_dir = /path/to/directory
+			crl_file = /path/to/file
+			weak_cert_validation = false
+			allow_invalid_hostname = false
+		}
 	}
 
 	radius_db = \"radius\"
@@ -46,13 +105,26 @@ template="sql {
 	groupreply_table = \"radgroupreply\"
 	usergroup_table = \"radusergroup\"
 	delete_stale_sessions = yes
+	pool {
+		start = \${thread[pool].start_servers}
+		min = \${thread[pool].min_spare_servers}
+		max = \${thread[pool].max_servers}
+		spare = \${thread[pool].max_spare_servers}
+		uses = 0
+		retry_delay = 30
+		lifetime = 0
+		idle_timeout = 60
+	}
+	client_table = \"nas\"
+	group_attribute = \"SQL-Group\"
+	\$INCLUDE \${modconfdir}/\${.:name}/main/\${dialect}/queries.conf
 }"
 
 sql_conf="$SCRIPT_DIR/sql"
 write_template_to_file "$template" "$sql_conf"
 delete_if_exists "/etc/freeradius/3.0/mods-enabled/sql"
-create_symbolic_link "$sql_conf" "/etc/freeradius/3.0/mods-enabled/sql-scansecurity" "freerad"
+create_symbolic_link "$sql_conf" "/etc/freeradius/3.0/mods-enabled/sql" "freerad"
 
 default_site="$SCRIPT_DIR/default"
 delete_if_exists "/etc/freeradius/3.0/sites-enabled/default"
-create_symbolic_link "$default_site" "/etc/freeradius/3.0/sites-enabled/default-scansecurity" "freerad"
+create_symbolic_link "$default_site" "/etc/freeradius/3.0/sites-enabled/default" "freerad"
--- nodogsplash/setup.sh
+++ nodogsplash/setup.sh
@@ -3,6 +3,9 @@
 # Cargando settings generales
 source "../config/config.txt"
 set_scriptdir "$BASH_SOURCE"
+# Cargando settings de red
+attempt_to_load "$AUTH_CONFIG"
+
 
 # Comprobar si nodogsplash se encuentra instalado
 if command -v nodogsplash &> /dev/null; then
@@ -31,13 +34,66 @@ fi
 # Cargando settings de red
 attempt_to_load "$NETWORK_CONFIG"
 
+auth_script="$SCRIPT_DIR/auth.sh"
+auth_link="/etc/nodogsplash/auth.sh"
+
+template="#!/bin/bash
+
+SHARED_SECRET='$SHARED_SECRET'
 
 
+echo \$@ >> /var/log/ndsauth.log
+
+echo \$SHARED_SECRET >> /var/log/ndsauth.log
+
+
+METHOD=\"\$1\"
+MAC=\"\$2\"
+
+case \"\$METHOD\" in
+  auth_client)
+    USERNAME=\"\$3\"
+    PASSWORD=\"\$4\"
+    radtest -x \$USERNAME \$PASSWORD localhost 1812 \$SHARED_SECRET >> /var/log/ndsauth.log
+    result=\$?
+    if [[ \$result -eq 0 ]]; then
+    
+      # Allow client to access the Internet for one hour (3600 seconds)
+      # Further values are upload and download limits in bytes. 0 for no limit.
+      echo 3600 0 0
+      exit 0
+    else
+      # Deny client to access the Internet.
+      echo \$result
+      exit 1
+    fi
+    ;;
+  client_auth|client_deauth|idle_deauth|timeout_deauth|ndsctl_auth|ndsctl_deauth|shutdown_deauth)
+    INGOING_BYTES=\"\$3\"
+    OUTGOING_BYTES=\"\$4\"
+    SESSION_START=\"\$5\"
+    SESSION_END=\"\$6\"
+    # client_auth: Client authenticated via this script.
+    # client_deauth: Client deauthenticated by the client via splash page.
+    # idle_deauth: Client was deauthenticated because of inactivity.
+    # timeout_deauth: Client was deauthenticated because the session timed out.
+    # ndsctl_auth: Client was authenticated by the ndsctl tool.
+    # ndsctl_deauth: Client was deauthenticated by the ndsctl tool.
+    # shutdown_deauth: Client was deauthenticated by Nodogsplash terminating.
+    ;;
+esac"
+
+write_template_to_file "$template" "$auth_script"
+chmod +x "$auth_script"
+create_symbolic_link $auth_script $auth_link "root"
 
 # Define the template for nodogsplash.conf
 template="GatewayInterface $AP_INTERFACE
 GatewayAddress $AP_IP
+GatewayName $AP_SSID
 GatewayPort 2050
+
+BinAuth $auth_link
 
 FirewallRuleSet authenticated-users {
     FirewallRule allow tcp port 53	
@@ -57,7 +113,10 @@ FirewallRuleSet users-to-router {
     FirewallRule allow udp port 67
 }
 
-MaxClients $AP_MAXCLIENTS"
+MaxClients $AP_MAXCLIENTS
+
+
+DebugLevel 3"
 
 
 nds_conf="$SCRIPT_DIR/nodogsplash.conf"
@@ -65,6 +124,17 @@ nds_conf="$SCRIPT_DIR/nodogsplash.conf"
 # Create the nodogsplash.conf file and substitute the values
 write_template_to_file "$template" "$nds_conf"
 create_symbolic_link "$nds_conf" "/etc/nodogsplash/nodogsplash.conf" "root"
+
+
+nds_splash_path="/etc/nodogsplash/htdocs/$NDS_PAGE"
+local_splash_path="$SCRIPT_DIR/$NDS_PAGE"
+create_symbolic_link "$local_splash_path" "$nds_splash_path" "root"
+
+nds_css_path="/etc/nodogsplash/htdocs/bootstrap.min.css"
+local_css_path="$SCRIPT_DIR/bootstrap.min.css"
+create_symbolic_link "$local_css_path" "$nds_css_path" "root"
+
+create_symbolic_link "$SCRIPT_DIR/images/$NDS_LOGO" "/etc/nodogsplash/htdocs/images/$NDS_LOGO" "root"
 
 # Check and add line "nodogsplash"
 add_to_rc_local "nodogsplash"
