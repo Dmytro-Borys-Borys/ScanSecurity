d2b39e9ecae81536f42b4d1082ecdd6189cfec68
Dmytro Borys <dimapotes@hotmail.com>
2023-06-12 16:24:18 +0200

    Comentarios y cambios cosméticos

--- python/setup.sh
+++ python/setup.sh
@@ -4,6 +4,7 @@
 source "$(dirname "$(readlink -f "$BASH_SOURCE")")/../config/config.env"
 SCRIPT_DIR="$(set_scriptdir "$BASH_SOURCE")"
 
+# Instalación de dependencias
 verify_dependency "command -v pip" "sudo apt install python3-pip -y"
 verify_pip_dependency "pyqrcode"
 verify_pip_dependency "escpos"
