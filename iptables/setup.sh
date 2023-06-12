c897514eeef4cc87e19a26aec48e31d57e555817
Dmytro Borys <dimapotes@hotmail.com>
2023-06-12 22:31:13 +0200

    fix iptables de cara a la presentación

--- iptables/setup.sh
+++ iptables/setup.sh
@@ -16,10 +16,7 @@ run "sudo iptables -v -F; \
     sudo iptables -v -t mangle -X; \
     sudo iptables -v -A FORWARD -i $GW_INTERFACE -o $AP_INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT; \
     sudo iptables -v -A FORWARD -i $AP_INTERFACE -o $GW_INTERFACE -j ACCEPT; \
-    sudo iptables -v -t nat -A POSTROUTING -o $GW_INTERFACE -j MASQUERADE; \
-    sudo iptables -v -P INPUT DROP; \
-    sudo iptables -v -P FORWARD DROP; \
-    sudo iptables -v -P OUTPUT DROP" "Ajustando iptables"
+    sudo iptables -v -t nat -A POSTROUTING -o $GW_INTERFACE -j MASQUERADE" "Ajustando iptables"
 
 
 # Guardar las reglas de iptables en un archivo
